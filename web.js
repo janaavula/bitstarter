46119ed0dc7998d3973432cad226699238cd3a95
AJ <jana.avula@gmail.com>
2013-07-10 23:13:42 +0000

    added web.js

--- web.js
+++ web.js
@@ -4,11 +4,11 @@ var app = express.createServer(express.logger());
 
 var fs = require("fs");
 
-var data = fs.readFileSync("index.html", "utf8");
+var buf = new Buffer(fs.readFileSync("index.html", "utf8");
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
-respond.send(data);
+//  response.send('Hello World2!');
+respond.send(buf.tostring());
 });
 
 var port = process.env.PORT || 5000;
